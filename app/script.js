c4f74643310020eb73e3dfd6f8d5979b8d2694d5
shawky55 <shakwysobhy98@gmail.com>
2022-03-02 23:00:43 +0200

    fix issues in Accessibilty

--- app/script.js
+++ app/script.js
@@ -21,19 +21,16 @@ const TODO_LIST = document.querySelector('.bottom-side .todo-list');
 const INFO = document.querySelector('.bottom-side .info');
 const MOBILE_VIEW = document.querySelector('.bottom-side .mobile-view');
 
-
 //Buttons Desktop
-const completeBtn = document.querySelector('.completed-button');
-const AllBtn = document.querySelector('.all-button');
-const clearCompletedBtn = document.querySelector('.clear-button');
-const ActiveBtn = document.querySelector('.Active-button');
+const COMPLETE_BTN = document.querySelector('.completed-button');
+const ALL_BTN = document.querySelector('.all-button');
+const CLEAR_COMPLETED_BTN = document.querySelector('.clear-button');
+const ACTIVE_BTN = document.querySelector('.Active-button');
 
 ///Buttons mobile view
 const ALL_MOBILE_BTN = document.querySelector('.all-mobile-btn');
 const ACTIVE_MOBILE_BTN = document.querySelector('.active-mobile-btn');
 const COMPLETED_MOBILE_BTN = document.querySelector('.completed-mobile-btn');
-
-
 
 let liCounter = 0;
 const emptyValue = '';
@@ -54,7 +51,7 @@ const insertTodo = (e) => {
   let todo = TODO_INPUT.value;
   if (todo == emptyValue) return;
   let node = document.createElement('li');
-  node.classList.add("notCompleted")
+  node.classList.add('notCompleted');
   node.id = `${++liCounter}`;
   node.innerHTML = `
                 <div class="checkBox">
@@ -83,7 +80,7 @@ const removeTodo = (e) => {
   info();
 };
 
-const info =()=> {
+const info = () => {
   let todoNum = todoUl.children.length;
   // let completedTodo = document.getElementsByClassName('checkBoxSelected');
   // let notCompleted = document.getElementsByClassName('notCompleted');
@@ -95,8 +92,7 @@ const info =()=> {
   } else {
     document.querySelector('.message').classList.remove('hidden');
   }
-}
-
+};
 
 const clearCompleted = (e) => {
   Array.from(todoUl.getElementsByClassName('missionCompleted')).forEach(
@@ -107,14 +103,12 @@ const clearCompleted = (e) => {
   info();
 };
 
-
 const AllTode = (e) => {
   Array.from(todoUl.children).forEach((element) => {
     element.classList.remove('hidden');
   });
   info();
 };
-
 
 const completedTodos = () => {
   Array.from(todoUl.children).forEach((element) => {
@@ -124,7 +118,6 @@ const completedTodos = () => {
   });
   info();
 };
-
 
 const ActiveTodo = (e) => {
   Array.from(todoUl.children).forEach((element) => {
@@ -137,18 +130,18 @@ const ActiveTodo = (e) => {
   info();
 };
 
-const selectTodos=(e)=>{
-if (e.target.classList.contains('check-icon')) {
-  let li = e.target.closest('li');
-  let checkBox = e.target.closest('.checkBox');
-  let icon = e.target;
-  li.classList.toggle('missionCompleted');
-  li.classList.toggle('notCompleted');
-  icon.classList.toggle('op');
-  checkBox.classList.toggle('checkBoxSelected');
-}
-info();//get new updates
-}
+const selectTodos = (e) => {
+  if (e.target.classList.contains('check-icon')) {
+    let li = e.target.closest('li');
+    let checkBox = e.target.closest('.checkBox');
+    let icon = e.target;
+    li.classList.toggle('missionCompleted');
+    li.classList.toggle('notCompleted');
+    icon.classList.toggle('op');
+    checkBox.classList.toggle('checkBoxSelected');
+  }
+  info(); //get new updates
+};
 ///
 FORM.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -172,22 +165,19 @@ INFO.addEventListener('click', function (e) {
   }
 });
 
+TODO_INPUT.addEventListener('blur', insertTodo);
 
-  TODO_INPUT.addEventListener('blur', insertTodo);
+THEME_CONTROL.addEventListener('click', modeToggle);
 
-  THEME_CONTROL.addEventListener('click', modeToggle);
+TODO_LIST.addEventListener('click', removeTodo);
 
-  TODO_LIST.addEventListener('click', removeTodo);
+todoUl.addEventListener('click', selectTodos);
 
-  todoUl.addEventListener('click', selectTodos);
-  
-clearCompletedBtn.addEventListener('click', clearCompleted);
-AllBtn.addEventListener("click",AllTode);
-ActiveBtn.addEventListener('click', ActiveTodo);
-completeBtn.addEventListener("click",completedTodos);
+CLEAR_COMPLETED_BTN.addEventListener('click', clearCompleted);
+ALL_BTN.addEventListener('click', AllTode);
+ACTIVE_BTN.addEventListener('click', ActiveTodo);
+COMPLETE_BTN.addEventListener('click', completedTodos);
 
-ALL_MOBILE_BTN.addEventListener('click',AllTode);
-COMPLETED_MOBILE_BTN.addEventListener("click",completedTodos);
-ACTIVE_MOBILE_BTN.addEventListener("click",ActiveTodo);
-
-
+ALL_MOBILE_BTN.addEventListener('click', AllTode);
+COMPLETED_MOBILE_BTN.addEventListener('click', completedTodos);
+ACTIVE_MOBILE_BTN.addEventListener('click', ActiveTodo);
