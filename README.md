8c814b45806dc99d2b18445c7cdcbd8c4ba548d0
shawky55 <shakwysobhy98@gmail.com>
2022-03-02 18:32:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Todo app solution
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+- small tode app with alot of features 
+- and organize todos well
+- app has light/Dark theme
+- Responsive with all different screen size
+- has button help you to get focus todo you want to look
+- soon i will add more feature to it 
+
+### Screenshot
+
+![Desktop-DarkTheme](./design/Desktop-Dark-1.png);
+![Desktoop-lightTheme](./design/Desktop-lightTheme.png)
+![mobile-lightTheme](./design/mobil-dark-1.png);
+![mobile-darkTheme](./design/mobile-light-1.png);
+![active-statue](./design/selected-itmes.png);
+
+
+### Links
+
+- Solution URL: [ solution URL ](https://your-solution-url.com)
+- Live Site URL: [ live site URL ](https://your-live-site-url.com)
+
+
+
---
+++ app/script.js
@@ -0,0 +1,193 @@
+'use strict';
+//two main section
+const TOP_SIDE = document.querySelector('.top-side');
+const BOTTOM_SIDE = document.querySelector('.bottom-side');
+///
+
+//Theme control
+const THEME_CONTROL = document.querySelector('.top-side .theme-control');
+const sunIcon = THEME_CONTROL.querySelector('.sun-icon');
+const moonIcon = THEME_CONTROL.querySelector('.moon-icon');
+
+//Form
+const TODO_INPUT = document.getElementById('todoInput');
+const FORM = document.querySelector('.top-side form');
+
+//Todo list
+
+const todoUl = document.querySelector('ul');
+
+const TODO_LIST = document.querySelector('.bottom-side .todo-list');
+const INFO = document.querySelector('.bottom-side .info');
+const MOBILE_VIEW = document.querySelector('.bottom-side .mobile-view');
+
+
+//Buttons Desktop
+const completeBtn = document.querySelector('.completed-button');
+const AllBtn = document.querySelector('.all-button');
+const clearCompletedBtn = document.querySelector('.clear-button');
+const ActiveBtn = document.querySelector('.Active-button');
+
+///Buttons mobile view
+const ALL_MOBILE_BTN = document.querySelector('.all-mobile-btn');
+const ACTIVE_MOBILE_BTN = document.querySelector('.active-mobile-btn');
+const COMPLETED_MOBILE_BTN = document.querySelector('.completed-mobile-btn');
+
+
+
+let liCounter = 0;
+const emptyValue = '';
+TODO_INPUT.focus();
+///
+const modeToggle = () => {
+  sunIcon.classList.toggle('hidden');
+  moonIcon.classList.toggle('hidden');
+  TOP_SIDE.classList.toggle('top-light-background');
+  FORM.classList.toggle('light-mode-bg');
+  BOTTOM_SIDE.classList.toggle('bottom-light-background');
+  TODO_LIST.classList.toggle('light-mode-bg');
+  INFO.classList.toggle('light-mode-bg');
+  MOBILE_VIEW?.classList.toggle('light-mode-bg');
+};
+
+const insertTodo = (e) => {
+  let todo = TODO_INPUT.value;
+  if (todo == emptyValue) return;
+  let node = document.createElement('li');
+  node.classList.add("notCompleted")
+  node.id = `${++liCounter}`;
+  node.innerHTML = `
+                <div class="checkBox">
+                    <img
+                      src="images/icon-check.svg"
+                      
+                      class="check-icon op"
+                      style="margin: 0px 2px"
+                      alt="check-icon"
+                    />
+                  </div>
+                  <div class="todo">${todo}</div>
+                  <img src="images/icon-cross.svg" alt="cross-icon" class="crossIcon" />
+                `;
+  todoUl.prepend(node);
+
+  TODO_INPUT.value = emptyValue;
+  info();
+};
+
+const removeTodo = (e) => {
+  if (e.target.className == 'crossIcon') {
+    let selectedLI = e.target.closest('li');
+    document.getElementById(selectedLI.id).remove();
+  }
+  info();
+};
+
+const info =()=> {
+  let todoNum = todoUl.children.length;
+  // let completedTodo = document.getElementsByClassName('checkBoxSelected');
+  // let notCompleted = document.getElementsByClassName('notCompleted');
+  document.querySelector('.todo-counter span').textContent = todoNum;
+  // document.getElementById('welcomMessage');
+  if (todoUl.getElementsByTagName('li').length) {
+    console.log(todoNum);
+    document.querySelector('.message').classList.add('hidden');
+  } else {
+    document.querySelector('.message').classList.remove('hidden');
+  }
+}
+
+
+const clearCompleted = (e) => {
+  Array.from(todoUl.getElementsByClassName('missionCompleted')).forEach(
+    (element) => {
+      element.remove();
+    }
+  );
+  info();
+};
+
+
+const AllTode = (e) => {
+  Array.from(todoUl.children).forEach((element) => {
+    element.classList.remove('hidden');
+  });
+  info();
+};
+
+
+const completedTodos = () => {
+  Array.from(todoUl.children).forEach((element) => {
+    if (element.classList.contains('notCompleted')) {
+      element.classList.toggle('hidden');
+    }
+  });
+  info();
+};
+
+
+const ActiveTodo = (e) => {
+  Array.from(todoUl.children).forEach((element) => {
+    if (element.classList.contains('missionCompleted')) {
+      element.classList.add('hidden');
+    } else {
+      element.classList.remove('hidden');
+    }
+  });
+  info();
+};
+
+const selectTodos=(e)=>{
+if (e.target.classList.contains('check-icon')) {
+  let li = e.target.closest('li');
+  let checkBox = e.target.closest('.checkBox');
+  let icon = e.target;
+  li.classList.toggle('missionCompleted');
+  li.classList.toggle('notCompleted');
+  icon.classList.toggle('op');
+  checkBox.classList.toggle('checkBoxSelected');
+}
+info();//get new updates
+}
+///
+FORM.addEventListener('submit', function (e) {
+  e.preventDefault();
+});
+
+TODO_INPUT.addEventListener('keyup', function (e) {
+  e.preventDefault();
+  if (e.keyCode === 13) {
+    insertTodo(e);
+  }
+});
+///
+INFO.addEventListener('click', function (e) {
+  console.log(e.target.tagName);
+  if (e.target.tagName == 'SMALL') {
+    INFO.querySelectorAll('small').forEach((btn) => {
+      btn.classList.remove('Active');
+      console.log('removed');
+    });
+    e.target.classList.add('Active');
+  }
+});
+
+
+  TODO_INPUT.addEventListener('blur', insertTodo);
+
+  THEME_CONTROL.addEventListener('click', modeToggle);
+
+  TODO_LIST.addEventListener('click', removeTodo);
+
+  todoUl.addEventListener('click', selectTodos);
+  
+clearCompletedBtn.addEventListener('click', clearCompleted);
+AllBtn.addEventListener("click",AllTode);
+ActiveBtn.addEventListener('click', ActiveTodo);
+completeBtn.addEventListener("click",completedTodos);
+
+ALL_MOBILE_BTN.addEventListener('click',AllTode);
+COMPLETED_MOBILE_BTN.addEventListener("click",completedTodos);
+ACTIVE_MOBILE_BTN.addEventListener("click",ActiveTodo);
+
+
